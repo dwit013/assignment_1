ecc5703a2d1925786bfef06398969ab868147a21
Abhishek Pandey <33335446+EnRyuuX7@users.noreply.github.com>
2018-11-04 10:23:48 +0545

    Update Assignment1_706.py
    Abhishek Pandey _ 706 _ Assignment 1

--- Assignment1_706.py
+++ Assignment1_706.py
@@ -1,6 +1,7 @@
 # Python Program to Calculate Compound interest
 # where P = Principal amount , t = no of years , r = Rate of Interest and n = no. of times interest is compounded per year 
 
+
 P = 3200
 r = 3.2
 t = 2
