d7e5568edef41465baca7594adf1e95e20249aa6
Rajat Raj Joshi <rajat8.rj@gmail.com>
2018-11-05 14:15:02 +0545

    assignment

---
+++ assignment1.py
@@ -0,0 +1,10 @@
+#name=Rajat Raj Joshi
+#roll=726
+#batch:2021 'B'
+
+p = float(input("Enter princial amount:"))
+r = float (input("Enter rate:"))
+t = float(input("Enter time:"))
+n = int (input("Enter no of times the interest is compounded per year:"))
+a = p*(1+(r/n))**(n*t)
+print("The amount is:",a)
