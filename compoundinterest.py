ccf1c6b79e815e63ea251c7fbbbca750f876ef13
Bhawana yadav <bhawana.yadav@gmail.com>
2018-11-05 10:55:25 +0545

    Compound Interest Program

---
+++ compoundinterest.py
@@ -0,0 +1,11 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 18:11:49) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> import math
+>>> p=100
+>>> r=5
+>>> n=2
+>>> t=2
+>>> A=p*math.pow(1+r/n,n*t)
+>>> A
+15006.25
+>>> 
